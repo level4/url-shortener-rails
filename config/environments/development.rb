2e078608def33c0e5ca4dcda79edc13654224392
James Tippett <james.tippett@gmail.com>
2018-05-22 14:23:02 +0800

    set base urls for redirect in environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # set the base URL for the redirect.
+  config.x.base_url = 'http://0.0.0.0:3000/'
+
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # set the base URL for the redirect.
+  config.x.base_url = 'http://url-shortenifier.herokuapp.com/'
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
