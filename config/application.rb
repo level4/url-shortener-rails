0d8b2414b5ab4ce5754abf24193c3a7926e87dc5
James Tippett <james.tippett@gmail.com>
2018-05-22 17:27:34 +0800

    change to a more heroku-compatible autoload strategy

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,9 @@ module UrlShortener
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-
-    config.autoload_paths += %W(
-      lib
-      lib/**/
-      serializers
-      ).map do |segment|
-      "#{config.root}/#{segment}"
-    end
+    load_path_strategy = Rails.env.production? ? :eager_load_paths : :autoload_paths
+    config.public_send(load_path_strategy) << Rails.root.join('lib')
+    config.public_send(load_path_strategy) << Rails.root.join('serializers')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
