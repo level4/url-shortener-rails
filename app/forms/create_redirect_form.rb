52d7b4be683bc094b47f1a86f51ef410dec76fcd
James Tippett <james.tippett@gmail.com>
2018-05-22 14:24:00 +0800

    add omitted create redirect form

--- app/forms/create_redirect_form.rb
+++ app/forms/create_redirect_form.rb
@@ -1,16 +1,38 @@
+require 'uri'
+
 class CreateRedirectForm
   include ActiveModel::Model
   include Dry::Monads::Either::Mixin
 
   attr_accessor :url
 
+  validates :url, presence: true
+  validate  :validate_url
+
   def process
+    normalize_url
     if valid?
+      Redirect.create!(url: url)
       Right(nil)
     else
       Left(errors)
     end
   end
 
+  # add protocol if missing. Go with http://
+  def normalize_url
+    parsed_url = URI.parse(url)
+    if parsed_url.scheme.nil?
+      @url = "http://#{url}"
+    end
+    rescue URI::InvalidURIError
+  end
+
+  def validate_url
+    parsed_url = URI.parse(url)
+    errors.add(:url, "Invalid URL") if parsed_url.host.nil?
+  rescue URI::InvalidURIError
+    errors.add(:url, "Invalid URL")
+  end
 
 end
